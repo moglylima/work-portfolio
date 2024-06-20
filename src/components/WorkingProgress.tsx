86eda773f183be7fd204775d0b9e138918f1161b
Mogleson Maciel <moglesonlima@gmail.com>
2024-06-20 15:47:06 -0300

    chore: remove sendmail.ts

---
+++ src/components/WorkingProgress.tsx
@@ -0,0 +1,11 @@
+
+export const WorkingProgress = () => {
+
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="text-center p-8 bg-white shadow-md rounded-lg">
+                <span className="text-9xl text-gray-700 font-bold blink2">Working in Progress!</span>
+            </div>
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 
 import Header from '@/components/Header';
+import { WorkingProgress } from '@/components/WorkingProgress';
 import Head from 'next/head';
 import React from 'react';
 
@@ -11,13 +12,7 @@ const Home = () => {
         <title>MoglyDev | Fullstack Dev</title>
       </Head>
       <Header />
-
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
-        <div className="text-center p-8 bg-white shadow-md rounded-lg">
-          <span className="text-9xl text-gray-700 font-bold blink2">Working in Progress!</span>
-        </div>
-      </div>
-
+      <WorkingProgress />
     </main>
   );
 };
